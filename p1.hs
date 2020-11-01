d63ac23ce8eac0feb33f2b5c34f9bb53f794c5dd
vpalmerini <victorpalmerini@gmail.com>
2020-11-01 13:38:23 -0200

    add float conversion and benford function

--- p1.hs
+++ p1.hs
@@ -1,14 +1,13 @@
--- Projeto 1
--- Victor Palmerini - 178061
-
 import Data.Char (isDigit)
-import Data.Map (fromList)
 import Data.Maybe (fromJust, isNothing)
+import Numeric
+
+formatFloatN floatNum numOfDecimals = showFFloat (Just numOfDecimals) floatNum ""
 
 split :: String -> [String]
 split [] = [""]
 split (c:cs)
-   | c == ' '  = "" : rest
+   | (c == ' ' || c == '\n')  = "" : rest
    | otherwise = (c : head rest) : tail rest
     where rest = split cs
 
@@ -25,6 +24,22 @@ updateMap key ((k,v):xs)
     | key == k = (k, v + 1):xs
     | otherwise = (k,v):(updateMap key xs)
 
+countDict [] = 0
+countDict ((k,v):xs) = countDict' ((k,v):xs) 0
+    where
+        countDict' [] acc = acc
+        countDict' ((k,v):xs) acc = countDict' xs (v + acc)
+
+parseDict [] _ = []
+parseDict ((k,v):xs) n = (k, (formatFloatN (v / n) 2)) : (parseDict xs n)
+
+benford [] dict = dict
+benford (a:as) dict
+    | isNothing (getFirstDigit a) = benford as dict
+    | otherwise = benford as (updateMap (fromJust (getFirstDigit a)) dict)
+
 main = do
         string <- getContents
-        putStrLn $ show $ benford $ split string
+        let dict = [('1', 0), ('2', 0), ('3', 0), ('4', 0), ('5', 0), ('6', 0), ('7', 0), ('8', 0), ('9', 0)]
+        let freq = benford (split string) dict
+        putStrLn $ show $ parseDict freq (countDict freq)
